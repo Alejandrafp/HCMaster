f590be283032e71e9853d21757f2e68c14cd89bc
SoniaMadrid <91297970+SoniaMadrid@users.noreply.github.com>
2023-05-28 00:43:16 -0600

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppLayoutModule } from './layout/app.layout.module';
-import { NotfoundComponent } from './demo/components/notfound/notfound.component';
-import { ProductService } from './demo/service/product.service';
-import { CountryService } from './demo/service/country.service';
-import { CustomerService } from './demo/service/customer.service';
-import { EventService } from './demo/service/event.service';
-import { IconService } from './demo/service/icon.service';
-import { NodeService } from './demo/service/node.service';
-import { PhotoService } from './demo/service/photo.service';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { DonacionesListaComponent } from './donaciones-lista/donaciones-lista.component';
+import { DonacionesFormularioComponent } from './donaciones-formulario/donaciones-formulario.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [
-        AppComponent, NotfoundComponent
-    ],
-    imports: [
-        AppRoutingModule,
-        AppLayoutModule
-    ],
-    providers: [
-        { provide: LocationStrategy, useClass: HashLocationStrategy },
-        CountryService, CustomerService, EventService, IconService, NodeService,
-        PhotoService, ProductService
-    ],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DonacionesListaComponent,
+    DonacionesFormularioComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/donacion.model.ts
@@ -0,0 +1,7 @@
+export interface Donacion {
+  id?: number;
+  nombreDonante?: string;
+  cantidadDonada?: number;
+  fecha?: Date;
+  proyectoId?: number;
+}
