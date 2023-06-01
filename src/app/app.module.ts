e917fc05d5e59fad5579da4f56d4bfb56b04c58f
SoniaMadrid <91297970+SoniaMadrid@users.noreply.github.com>
2023-05-31 22:44:18 -0600

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { DonacionesListComponent } from './donaciones-list/donaciones-list.component';
-import { DonacionesDetailComponent } from './donaciones-detail/donaciones-detail.component';
-import { DonacionesCreateComponent } from './donaciones-create/donaciones-create.component';
-import { DonacionesEditComponent } from './donaciones-edit/donaciones-edit.component';
+import { DonacionesComponent } from './donaciones/donaciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DonacionesListComponent,
-    DonacionesDetailComponent,
-    DonacionesCreateComponent,
-    DonacionesEditComponent
+    DonacionesComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    HttpClientModule, FormsModule, BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
