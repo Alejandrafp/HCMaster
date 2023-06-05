e5428131f39f0028fd80fd6a257a0f8be9cb3553
SoniaMadrid <91297970+SoniaMadrid@users.noreply.github.com>
2023-06-04 18:04:27 -0600

    Add files via upload

--- src/app/voluntarios.service.ts
+++ src/app/voluntarios.service.ts
@@ -1,28 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Voluntario } from './voluntarios/voluntario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoluntariosService {
-  private apiUrl = 'API'; //  aqui va el API t.t
+  readonly HCMasterURL = "https://localhost:7036/api"
 
   constructor(private http: HttpClient) { }
 
   obtenerVoluntarios(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/voluntarios`);
+    return this.http.get<any[]>(`${this.HCMasterURL}/voluntarios`);
   }
 
   crearVoluntario(voluntario: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/voluntarios`, voluntario);
+    return this.http.post<any>(`${this.HCMasterURL}/voluntarios`, voluntario);
   }
 
   actualizarVoluntario(id: number, voluntario: any): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/voluntarios/${id}`, voluntario);
+    return this.http.put<any>(`${this.HCMasterURL}/voluntarios/${id}`, voluntario);
   }
 
   eliminarVoluntario(id: number): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}/voluntarios/${id}`);
+    return this.http.delete<any>(`${this.HCMasterURL}/voluntarios/${id}`);
   }
 }
