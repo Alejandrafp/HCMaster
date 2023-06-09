ee4bab326c3a040bcabc72596e1c4de23d92163a
SoniaMadrid <91297970+SoniaMadrid@users.noreply.github.com>
2023-06-09 01:02:15 -0600

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { InspectionApiService } from './inspection-api.service';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppLayoutModule } from './layout/app.layout.module';
+import { ProductService } from './demo/service/product.service';
+import { CountryService } from './demo/service/country.service';
+import { CustomerService } from './demo/service/customer.service';
+import { EventService } from './demo/service/event.service';
+import { IconService } from './demo/service/icon.service';
+import { NodeService } from './demo/service/node.service';
+import { PhotoService } from './demo/service/photo.service';
+import { DonacionesComponent } from './donaciones/donaciones.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        DonacionesComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        AppLayoutModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    providers: [
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        CountryService, CustomerService, EventService, IconService, NodeService,
+        PhotoService, ProductService
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ layout/app.menu.component.ts
@@ -0,0 +1,83 @@
+import { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { LayoutService } from './service/app.layout.service';
+
+@Component({
+    selector: 'app-menu',
+    templateUrl: './app.menu.component.html'
+})
+export class AppMenuComponent implements OnInit {
+
+    model: any[] = [];
+
+    constructor(public layoutService: LayoutService) { }
+
+    ngOnInit() {
+        this.model = [
+            {
+                label: 'Inicio',
+                items: [
+                    { label: 'Bienvenido', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
+                ]
+            },
+            {
+                label: 'Herramientas',
+                icon: 'pi pi-fw pi-briefcase',
+                items: [
+                    {
+                        label: 'Auth',
+                        icon: 'pi pi-fw pi-user',
+                        items: [
+                            {
+                                label: 'Login',
+                                icon: 'pi pi-fw pi-sign-in',
+                                routerLink: ['/auth/login']
+                            },
+                            {
+                                label: 'Access Denied',
+                                icon: 'pi pi-fw pi-lock',
+                                routerLink: ['/auth/access']
+                            }
+                        ]
+                    },
+                    {
+                        label: 'Crud',
+                        icon: 'pi pi-fw pi-pencil',
+                        routerLink: ['/pages/crud']
+                    },
+                    {
+                        label: 'Usuario',
+                        icon: 'pi pi-user-plus',
+                        routerLink: ['/pages/usuario']
+                    },
+                    {
+                        label: 'Organización',
+                        icon: 'pi pi-sitemap',
+                        routerLink: ['/pages/organizacion']
+                    },
+                    {
+                        label: 'Proyectos',
+                        icon: 'pi pi-wrench',
+                        routerLink: ['/pages/proyectos']
+                    },
+                    {
+                        label: 'Marketing',
+                        icon: 'pi pi-wrench',
+                        routerLink: ['/pages/marketing']
+                    },
+                    {
+                        label: 'Clientes',
+                        icon: 'pi pi-wrench',
+                        routerLink: ['/pages/clientes']
+                    },
+                    {
+                        label: 'Competencias',
+                        icon: 'pi pi-wrench',
+                        routerLink: ['/pages/competencias']
+                    },
+                        
+                ]
+            }
+        ];
+    }
+}
